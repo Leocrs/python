2710e5393cc491a02673a4cbc5fa877b0f3f4634
leonardo <leonnardo.campos@gmail.com>
2023-05-09 10:31:07 -0300

    first commit

---
+++ mapa.py
@@ -0,0 +1,10 @@
+"""
+   Mapas São conhecidos em Python como dicionario
+    
+    """
+    
+receita = {'jan': 100, 'fev': 120, 'mar': 300}
+
+print(receita)
+print(receita['fev'])
+print(receita.get('fev'))
