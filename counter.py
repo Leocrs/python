4f5b78b64fcb2aba3f9f4e58cd43c70e8d1b0ba7
leonardo <leonnardo.campos@gmail.com>
2023-05-11 17:19:00 -0300

    Update counter.py

--- counter.py
+++ counter.py
@@ -1,10 +1,14 @@
 """
-Modulo collection - Counter (contador)
+Modulo collection - Counter (contador) High performance Container performance
 
-
-
-
-
-
+Ele recebe um dicionário como parâmetro e retorna um objeto do tipo Counter, que é parecido com um dicionário
 
     """
+    
+from collections import Counter # importa a biblioteca
+
+lista = [1, 2, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10] # lista com números
+
+contador = Counter(lista) # cria um objeto do tipo Counter
+print(contador) # imprime o contador
+print(type(contador)) # imprime o tipo
