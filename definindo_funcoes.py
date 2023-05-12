2ed7d252c782f93c3c14bc6ee2d6531241eb78ff
leonardo <leonnardo.campos@gmail.com>
2023-05-12 15:51:25 -0300

    Update definindo_funcoes.py

--- definindo_funcoes.py
+++ definindo_funcoes.py
@@ -2,5 +2,23 @@
 Definindo funções, funções anônimas e lambda
 Funções são pequenos trechos de código que podem ser reutilizados em tarefas específicas.
         
+ Varias funções:
+ print()
+ len()
+ max()
+ min()
+ count()
+               
     """
     
+#Exemplo 1 de utilizando funções:
+
+cores = ['vermelho', 'azul', 'amarelo', 'verde']
+
+#utilizando a função integrada no Python
+print(cores)
+cores.append('roxo')
+print(type(cores))
+print(cores)
+print(len(cores))
+print(cores[2])
