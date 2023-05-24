72b8488c9bdde17ff217a25b1f1deb645431bbf0
Leonardo Campos <49033393+Leocrs@users.noreply.github.com>
2023-05-23 23:51:40 -0300

    Update map.py

--- map.py
+++ map.py
@@ -19,13 +19,35 @@ print(areas)
 print(list(map(area, raios))) # list comprehension
 
 print(list(map(lambda raio: math.pi * raio**2, raios))) # função lambda
-"""
-
-# Para fixr Map temos dados iteraveis, dados a1, a2, a3, ... an. Temos ums função f(a1), f(a2), f(a3), ... 
+ 
+Para fixr Map temos dados iteraveis, dados a1, a2, a3, ... an. Temos ums função f(a1), f(a2), f(a3), ... 
 #Utilizamos o map para fazer a função f(a1), f(a2), f(a3), ... 
-# O Map object retorna: uma lista com os valores da função f(a1), f(a2), f(a3), ...
+O Map object retorna: uma lista com os valores da função f(a1), f(a2), f(a3), ...
 
-# exemplo 2 
+xemplo 2 
 
 cidades_lista = [('São Paulo',29),('Rio de Janeiro',30), ('Belo horizonte',27), ('Porto Alegre18',15)]
 print(list(map(lambda cidade: cidade, cidades_lista)))
+
+
+precos = [1000, 1500, 1250, 2500]
+def adiciona_imposto (preco):
+    return preco *1.1
+preco_com_imposto = []
+for preco in precos:
+    preco_com_imposto.append(adiciona_imposto(preco))
+print(preco_com_imposto)
+
+
+
+
+precos = [1000, 1500, 1250, 2500]
+precos_com_imposto = list(map(lambda x: x * 1.1, precos)) # Aplica o imposto de 10% em cada valor da lista
+precos_formatados = ["R${:,.2f}".format(valor) for valor in precos_com_imposto] # Formata cada valor da lista como uma string em moeda brasileira
+print(precos_formatados)
+
+"""
+
+import pandas 
+
+
