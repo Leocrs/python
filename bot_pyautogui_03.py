07b371aefd6bc2b9711c3f15a7fbea8667af6cea
leonardo <leonnardo.campos@gmail.com>
2023-05-26 13:24:58 -0300

    up

--- bot_pyautogui_03.py
+++ bot_pyautogui_03.py
@@ -1,51 +1,43 @@
-# Programa de BOT de backup automatico de arquivos
+#Programa de BOT de backup automatico de arquivos
 
 import pyautogui
 import time
 import os # Biblioteca para manipulação de arquivos
 import shutil # Biblioteca para manipulação de arquivos
-pyautogui.PAUSE = 1 # tempo de pausa segundos entre tentativas
-pyautogui.alert(" O codigo vai começar, Não use nada do seu computador enquanto o codigo esiver rodando!!!")
+pyautogui.PAUSE = 3 # tempo de pausa segundos entre tentativas
+pyautogui.alert(" O programa vai começar, aguarde...")
+
 
 # abrir o gooqle drive na pasta de backup
 pyautogui.press('winleft') # clica na tecla windows da esquerda
 pyautogui.write('chrome') # escreva o nome do arquivo
 pyautogui.press('enter') # clica no enter
-time.sleep(1) # tempo de pausa
+time.sleep(1)
 pyautogui.write('https://drive.google.com/drive/folders/1BK33oZWE0WkzPpA8cdiWIOqe58ezU-QF') # escreva o link do arquivo
 pyautogui.press('enter') # clica no enter
+time.sleep(1)
 
-
-#  entrar na pasta do arquivos copiar e color no google drive
+# entrar na pasta do arquivos copiar e color no google drive
 pyautogui.hotkey('winleft', 'r')
 time.sleep(1)
 pyautogui.typewrite(r" C:\Users\leonardo\Documents\backup ") # OBS: "r" é necessario para o Python abrir qualquer caminho sem inventar "moda"
 pyautogui.press('enter')
 pyautogui.hotkey('ctrl', 'a')
-pyautogui.hotkey('ctrl', 'c')
-pyautogui.write('https://drive.google.com/drive/folders/1BK33oZWE0WkzPpA8cdiWIOqe58ezU-QF') # escreva o link do arquivo
+pyautogui.dragTo(x=1133, y=342, duration=2)
 time.sleep(1)
-# Pressiona a tecla "Alt"
-pyautogui.keyDown('altgr')
-pyautogui.hotkey('tab', 'tab')
+pyautogui.mouseDown()
+pyautogui.dragTo(x=550, y=591, duration=2)
+pyautogui.hotkey('alt','tab')
+time.sleep(1)
+pyautogui.mouseUp()
+pyautogui.alert("O programa terminou!")
 
 
-
-
-
-
-
-# Itera pelo atalho "Alt + Tab + Tab" duas vezes
-# pyautogui.press('tab')
-# time.sleep(0.5)
-# pyautogui.press('tab')
-# # Solta a tecla "Alt"
-# pyautogui.keyUp('alt')
-
-
-#pyautogui.hotkey('ctrl', 'v') 
-pyautogui.press('enter') # copia o arquivo
-pyautogui.alert("O programa terminou de rodar!")
+#Comando nessessario para copiar o arquivo na posição exata via captura de tela
+# time.sleep(5)
+# from turtle import position
+# posicao = pyautogui.position()
+# print(posicao)
 
 
 
