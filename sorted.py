947bad7c68963e9fc73deab44db777247233102d
leonardo <leonnardo.campos@gmail.com>
2023-05-29 11:20:35 -0300

    up

--- sorted.py
+++ sorted.py
@@ -7,14 +7,16 @@ sorted é uma função que retorna uma lista ordenada.
 """
 #exemplo 1
 
-nomes = ['João', 'Maria', 'José', 'Ana']
+nomes = ('João', 'Maria', 'José', 'Ana, karina')
 print(sorted(nomes))
 
 
-nomes = ('João', 'Maria', 'José', 'Ana')
+nomes = {'João', 'Maria', 'José', 'Ana, karina'}
 print(sorted(nomes))
 
-nomes = {'João', 'Maria', 'José', 'Ana'}
-print(sorted(nomes))
+nomes = ['João', 'Maria', 'José', 'Ana, karina']
 print(sorted(nomes, reverse=True))
 print(sorted(nomes, key=len))
+print(sorted(nomes, key=max))
+
+
