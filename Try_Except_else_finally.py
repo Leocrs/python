fa2c55156c842a67458b1c512d9ad5736b0fecc9
leonardo <leonnardo.campos@gmail.com>
2023-05-30 14:14:46 -0300

    up

--- Try_Except_else_finally.py
+++ Try_Except_else_finally.py
@@ -29,3 +29,25 @@ finally:
     print('Fim do programa!')   #OBS: Sempre será executado ao final do programa dando erro ou não
     
 
+# try/except - tratamento de erros e exceções 
+
+# Utilizamos o bloco para tratarmos erros e exceções.
+
+# try:
+#      //execução do bloco problema
+# except:
+#     //tratamento que deve ser feito caso ocorra algum problema
+    
+    #exemplo 1
+try:
+    len()
+except:
+    print('Deu algum problema')
+# Tente executar a função leo() e verifique se ocorreu algum problema. caso ocorra, execute o bloco except print('Deu algum problema')
+    
+#exemplo 2
+
+try:
+    len(5)
+except TypeError:
+    print('Você está tentando executar uma função que nao existe')
