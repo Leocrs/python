07b2ffa5e6b10fe1e4d3d422a194936af12ed6c5
leonardo <leonnardo.campos@gmail.com>
2023-05-31 16:53:13 -0300

    up

---
+++ iterators_iterables.py
@@ -0,0 +1,6 @@
+# iterators and iterables
+# iterators é um objeto que pode ser iterado, sendo uma estrutura de dados que permite a iteração de valores por vez
+# iterables é um objeto que pode ser iterado, quando a função iter for chamada
+
+nome = 'Leonardo'
+numeros = [1, 2, 3, 4, 5]
