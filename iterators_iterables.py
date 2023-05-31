9a6052bf72c4f08ca1fe65c32c27bc520c11a236
leonardo <leonnardo.campos@gmail.com>
2023-05-31 17:14:35 -0300

    up

--- iterators_iterables.py
+++ iterators_iterables.py
@@ -1,6 +1,25 @@
 # iterators and iterables
-# iterators é um objeto que pode ser iterado, sendo uma estrutura de dados que permite a iteração de valores por vez
-# iterables é um objeto que pode ser iterado, quando a função iter for chamada
+# iterators é um objeto que pode ser iterado, sendo uma estrutura de dados que permite a iteração de uma função next() 
+# iterables é um objeto que pode ser iterado, quando a função iter() for chamada
 
-nome = 'Leonardo'
-numeros = [1, 2, 3, 4, 5]
+nome = 'Leonardo' # iterable mas não é um iterador
+numeros = [1, 2, 3, 4, 5] # iterable mas não é um iterador
+
+it1 = iter(nome)
+it2 = iter(numeros)
+
+print(next(it1))
+print(next(it1))
+print(next(it1))
+
+print(next(it2))
+print(next(it2))
+print(next(it2))
+print(next(it2))
+print(next(it2))
+
+
+
+
+    
+
--- seek_redline_with_StringIO_.py
+++ seek_redline_with_StringIO_.py
@@ -2,13 +2,13 @@
 
 # Forma de abrir arquivos em Python
 
-# arquivo = open('Texto_Basico.txt', 'r' ) # abre o arquivo codigos_moedas.txt no modo de leitura
-# print(arquivo) # <_io.TextIOWrapper name='codigos_moedas.txt' mode='r' encoding='UTF-8'> 
-# print(arquivo.read())    # codigos_moedas modo de leitura
-# print(arquivo.readline()) # codigos_moedas modo de leitura 1 linha
-# print(type(arquivo.readline())) # <class 'str'> 
-# arquivo.seek(0) # para pular uma linha do arquivo
-# print(arquivo.readlines()) #redlines() - para ler o arquivo no modo de leitura linha a linha
+arquivo = open('Texto_Basico.txt', 'r' ) # abre o arquivo codigos_moedas.txt no modo de leitura
+print(arquivo) # <_io.TextIOWrapper name='codigos_moedas.txt' mode='r' encoding='UTF-8'> 
+print(arquivo.read())    # codigos_moedas modo de leitura
+print(arquivo.readline()) # codigos_moedas modo de leitura 1 linha
+print(type(arquivo.readline())) # <class 'str'> 
+arquivo.seek(0) # para pular uma linha do arquivo
+print(arquivo.readlines()) #redlines() - para ler o arquivo no modo de leitura linha a linha
 
 # ret retorna a quantidade de linhas do arquivo 
 # redline() - para ler o arquivo no modo de leitura linha a linha
@@ -32,40 +32,39 @@
 # arquivo.write('Python') # escreve no arquivo codigos_moedas.txt
 
 #exemplo 1 
-# arquivo = open('Texto_Basico.txt', 'w' ) # abre o arquivo codigos_moedas.txt no modo de escrita
-# print(arquivo.write('Python')) # escreve no arquivo 
+arquivo = open('Texto_Basico.txt', 'w' ) # abre o arquivo codigos_moedas.txt no modo de escrita
+print(arquivo.write('Python')) # escreve no arquivo 
 
 #OBS: write() - escreve no arquivo em novo arquivo caso eu queira escrever numa boa, 
 # porem se eu tentar escrever em um arquivo já existente, o python apaga o texto anterior.
 
 #exemplo 2
 
-# with open('Texto_Basico.txt', 'w') as arquivo: # abre o arquivo codigos_moedas.txt no modo de escrita
-#     print(arquivo.write('Escrever dados em arquivo é muito facil. \n'))
-#     print(arquivo.write('Essa é a ultima linha escrita de texto. \n'))
+with open('Texto_Basico.txt', 'w') as arquivo: # abre o arquivo codigos_moedas.txt no modo de escrita
+    print(arquivo.write('Escrever dados em arquivo é muito facil. \n'))
+    print(arquivo.write('Essa é a ultima linha escrita de texto. \n'))
     
-# with open('Texto_Basico.txt', 'r') as arquivo: # abre o arquivo codigos_moedas.txt no modo de leitura
-#     print(arquivo.read())
+with open('Texto_Basico.txt', 'r') as arquivo: # abre o arquivo codigos_moedas.txt no modo de leitura
+     print(arquivo.read())
 
 # 'x' - para escrever no arquivo CASO ELE não EXISTIR!
 # 'a' - para escrever no arquivo CASO ELE EXISTIR
 
-
-# with open('Texto_teste.txt', 'w') as arquivo1:
-#     print(arquivo1.write('Escrever dados em arquivo para teste! \n'))
-#     print(arquivo1.readline)
+with open('Texto_teste.txt', 'w') as arquivo1:
+    print(arquivo1.write('Escrever dados em arquivo para teste! \n'))
+    print(arquivo1.readline)
 #######################################################################################################
 
-# Forma de abrir arquivos em Python do Sistema Operacional
-# import da biblioteca io EX: from io import StringIO
+#  Forma de abrir arquivos em Python do Sistema Operacional
+#  import da biblioteca io EX: from io import StringIO
 
-# from io import StringIO
+from io import StringIO
 
-# mensagem = 'Python é uma linguagem de programação de alto nível' # mensagem do arquivo
-# arquivo = StringIO(mensagem) # cria um objeto StringIO
-# arquivo = open('arquivo.txt', 'w') # abre o arquivo codigos_moedas.txt no modo de leitura
-# print(arquivo.read())
+mensagem = 'Python é uma linguagem de programação de alto nível' # mensagem do arquivo
+arquivo = StringIO(mensagem) # cria um objeto StringIO
+arquivo = open('arquivo.txt', 'w') # abre o arquivo codigos_moedas.txt no modo de leitura
+print(arquivo.read())
 
-# arquivo.write('Python é uma linguagem de programação de alto nível')
+arquivo.write('Python é uma linguagem de programação de alto nível')
 
 
