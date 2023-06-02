ea3b62d4bee8366c7a9afbbb0c1c365c8c51f46c
leonardo <leonnardo.campos@gmail.com>
2023-06-02 13:44:53 -0300

    up

--- teste_de_velocidade.py
+++ teste_de_velocidade.py
@@ -3,9 +3,10 @@
 # Teste de velocidade com expressões geradoras 
 
 import time # importa a biblioteca time
+
 #Generador com expressões geradoras (generator expressions)
 gen_inicio = time.time() # tempo decorrido
-print(sum(numero for numero in range(1, 11))) # soma dos numeros
+print(sum(numero for numero in range(100))) # soma dos numeros
 gen_tempo = time.time() - gen_inicio # tempo decorrido
 print(gen_tempo) # tempo decorrido
 
@@ -14,10 +15,11 @@ print(gen_tempo) # tempo decorrido
 #list comprehension
 
 list_iniicio = time.time()
-print(sum([numero for numero in range(100)])) # 100 milhões de numeros
+print(sum([numero for numero in range(100)])) # 100 de numeros
 list_tempo = time.time() - list_iniicio
-print(list_tempo)
-
 
 print(f'generator expression levou {gen_tempo}')
 print(f'list comprehension levou {list_tempo}')
+
+######################################################################################################################
+
