85377e02c40bce6245f15ea03c74815681049643
leonardo <leonnardo.campos@gmail.com>
2023-06-05 17:22:30 -0300

    up

--- decorator.py
+++ decorator.py
@@ -1,6 +1,19 @@
 import requests
+import time
 
-def pegar_cotacao_dolar():
+# Criar um decorador calcular_tempo
+def calcular_tempo(funcao):  # funcao recebe uma funcao
+    def wrapper():
+        tempo_inicial = time.time()
+        funcao()
+        tempo_final = time.time()
+        print(f'A duração foi de {tempo_final - tempo_inicial} segundos') # tempo_final - tempo_inicial = tempo decorado
+    return wrapper
+
+      
+    
+@calcular_tempo    
+def pegar_cotacao_dolar(): # pegar_cotacao_dolar recebe uma funcao
     link = f"https://economia.awesomeapi.com.br/last/USD-BRL"
     requisicao = requests.get(link)
     requisicao = requisicao.json()
--- site.py
+++ site.py
@@ -2,8 +2,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
+@app.route('/')
 def homepage():
     return 'Essa é minha HomePage'
+
+@app.route('/contato')
+def contatos():
+    return 'Esses são os meus contatos'
 
 
 app.run()
