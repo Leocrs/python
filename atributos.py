0c64d83843c51fecd43ba83194093e8ca4b5516d
Leonardo Campos <49033393+Leocrs@users.noreply.github.com>
2023-06-15 17:55:45 -0300

    up

--- atributos.py
+++ atributos.py
@@ -2,3 +2,9 @@
 
 # Atributos é uma característica de uma classe.
 
+# Em Python, os atributos são chamados de propriedades. e dividos em 3 grupos
+# 1) Atributos de instância
+# 2) Atributos de classe
+# 3) Atributos de dinamicos
+
+# 
