51e13bbb25c12764576be86ce08ee46c80ec5f90
leonardo <leonnardo.campos@gmail.com>
2023-06-28 11:39:50 -0300

    up

--- atributos.py
+++ atributos.py
@@ -3,7 +3,34 @@
 # Atributos é uma característica de uma classe.
 
 # Em Python, os atributos são chamados de propriedades. e dividos em 3 grupos
-# 1) Atributos de instância
-# 2) Atributos de classe
-# 3) Atributos de dinamicos
+
+
+# Atributos de instância: exemplos:
+
+
+# Atributos de classe: Exemplos: 
+# Por convenção , os atributos de classe são chamados publicos. Ou seja pode ser acessado por todo o projeto, Agora caso queria que o atributo fosse privado, 
+# deveria ser declarado como privado por duplo underscore "__" no ínicio do atributo. Isso tbm é conhecido como Mangling.
+
+
+class Lampada:
+    def __init__(self, cor, voltagem):
+        self.cor = cor
+        self.ligada = False
+        self.voltagem = voltagem 
+        
+class Usuario:
+    def __init__(self, nome, senha):
+        self.nome = nome
+        self.senha = senha
+        
+class Produto:
+    def __init__(self, nome, preco):
+        self.nome = nome
+        self.preco = preco
+        
+        
+
+
+# Atributos de dinamicos: Exemplos:
 
