93518a9f18425c8aa29910080dc2feb175ad0ba8
leonardo <leonnardo.campos@gmail.com>
2023-06-29 09:36:31 -0300

    up

---
+++ metodos.py
@@ -0,0 +1,5 @@
+
+
+#POO Métodos
+
+# Métodos (função) representam um conjunto de acções que podem ser realizadas em um objeto. Ou seja um método é um conjunto de acções que podem ser executadas em um objeto.
