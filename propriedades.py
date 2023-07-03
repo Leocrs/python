ecdf801d22fc216ab5bd808d7da167bc4ed3ac73
leonardo <leonnardo.campos@gmail.com>
2023-07-03 16:29:49 -0300

    up

--- propriedades.py
+++ propriedades.py
@@ -1,2 +1,14 @@
 #POO Propriedades properties de uma classe
 
+class Conta:
+    contador =0
+    
+    def __init__(self, titular, saldo, limite):
+        self.titular = titular
+        self.saldo = saldo
+        self.limite = limite
+        Conta.contador += 1
+    
+    def extrato(self):
+        return f'Saldo de {self.saldo} do clinte {self.titular}'
+    
