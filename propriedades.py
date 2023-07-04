379e86b218837dc630e5aa26363859402c9abef2
leonardo <leonnardo.campos@gmail.com>
2023-07-04 11:34:37 -0300

    up

--- propriedades.py
+++ propriedades.py
@@ -8,7 +8,43 @@ class Conta:
         self.saldo = saldo
         self.limite = limite
         Conta.contador += 1
+        
+@property
+def numero(self):
+    return self.__numero
+@property
+def saldo(self):
+    return self.__saldo
+@property
+def titular(self):
+    return self.__titular
+@property
+def limite(self):
+    return self.__limite
+
+def extrato(self):
+    return f'Saldo de {self.saldo} do clinte {self.titular}'  
+def depositar(self, valor):
+    self.__saldo += valor     
+def sacar(self, valor):
+    self.__saldo -= valor    
+def transfer(self, valor, destino):
+    self.__saldo -= valor
+    destino.__saldo += valor 
     
-    def extrato(self):
-        return f'Saldo de {self.saldo} do clinte {self.titular}'
+@property
+def valor_total(self):
+    return self.__valor_total + self.__limite
+    
+@limite.setter
+def limite(self, novo_valor):
+    self.__limite = novo_valor
+
+conta1 = Conta('Leonardo', 1000, 500)
+conta2 = Conta('Maria', 2000, 700)
+
+soma = conta1.saldo + conta2.saldo
+print(f'A soma do saldo das contas é {soma}')
+print(conta1.limite)
+
     
