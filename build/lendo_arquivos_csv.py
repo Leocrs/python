39d524934849e59221409da1e557bb511b45081c
leonardo <leonnardo.campos@gmail.com>
2023-07-05 14:26:04 -0300

    up

---
+++ build/lendo_arquivos_csv.py
@@ -0,0 +1,2 @@
+# Lendo arquivos CSV - Python 
+
--- build/metodos_magicos.py
+++ build/metodos_magicos.py
@@ -10,8 +10,11 @@ class Livro:
     def __repr__(self): # __repr__  é o método 
         return f"Titulo: {self.titulo}, Autor: {self.autor}, Paginas: {self.paginas}" 
     
-    def __len__ (self):
+    def __len__ (self): # __len__ é o método usando o len para saber o tamanho de um atributo da classe
         return self.paginas
+    
+    def __add__(self,outro): # __add__ é o método para somar dois atributos da classe
+        return f'{self.titulo} e{outro.titulo}'
                
 livro1 = Livro("O Senhor dos Aneis", "J.R.R. Paginas", 400)
 livro2 = Livro(" Nas Estrelas", "J.R.R. Paginas", 300)
@@ -20,3 +23,5 @@ print(livro1)
 print(livro2)
 print(len(livro1))
 print(len(livro2))
+print(livro1 + livro2)
+
