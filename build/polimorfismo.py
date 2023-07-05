9d3fd55ef75e495d278032aeb08c368ff15cb1bc
leonardo <leonnardo.campos@gmail.com>
2023-07-05 10:47:03 -0300

    up

--- build/polimorfismo.py
+++ build/polimorfismo.py
@@ -2,3 +2,16 @@
 # podem ter diferentes tipos de atributos e métodos   
 
 
+class Animal(object):
+    def __init__(self, nome):
+        self.__nome = nome
+        
+    def falar(self,):
+        raise NotImplementedError("Método não implementado, apenas para herança de classe FILHA")
+    
+    def comer (self,):
+        print(f'{self.__nome} está comendo')
+        
+           
+        
+    
