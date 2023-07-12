12c28d725fcc61ddd1ebd377ec6c1f1a4449c55d
leonardo <leonnardo.campos@gmail.com>
2023-07-12 16:46:51 -0300

    up

--- build/metodos_data_hora.py
+++ build/metodos_data_hora.py
@@ -7,3 +7,15 @@ print(agora)
 print(agora.strftime('%d/%m/%Y, %H:%M:%S')) # Data e hora no Python atual formatada par BR
 print(type(agora))
 
+hoje = datetime.datetime.today() # Data e hora no Python HOJE
+print(hoje)
+print(hoje.strftime('%d/%m/%Y, %H:%M:%S')) # Data e hora no Python atual
+print(repr(hoje))
+
+
+# Mudanças ocorrendo a meia-noite a combinar (hora e minuto) Exemplo para manutenção de backup etc.
+manutencao= datetime.datetime.combine(datetime.datetime.now() + datetime.timedelta(days=1), datetime.time())
+print(manutencao)
+print(manutencao.strftime('%d/%m/%Y, %H:%M:%S'))
+
+
