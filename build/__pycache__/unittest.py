8c8cdf7dc5af18b6be14ae40890469c280769a03
leonardo <leonnardo.campos@gmail.com>
2023-07-19 13:04:53 -0300

    up

--- build/__pycache__/unittest.py
+++ build/__pycache__/unittest.py
@@ -1,2 +1,15 @@
 # Unittest - Test Unitário 
 
+import unittest
+
+from atividades import comer, dormir
+ 
+class AtividadesUnitest(unittest.TestCase):
+  pass
+
+if __name__ == '__main__':
+  unittest.main()
+  
+  
+#Testcase é um teste unitário 
+
