8f63ab4b6f74874ce9b3a539cc138af879b61e6c
leonardo <leonnardo.campos@gmail.com>
2023-07-20 08:38:02 -0300

    up

--- build/__pycache__/atividades.py
+++ build/__pycache__/atividades.py
@@ -4,3 +4,12 @@ def comer (comida, eh_saudavel):
 def dormir(sono, horas):
     pass
 
+def eh_engracada (feliz, alegre):
+    pass
+
+def pessoa (nome, idade):
+    if nome == " Leonardo":
+        return False
+    return True
+  
+    
--- build/__pycache__/modo_unittest.py
+++ build/__pycache__/modo_unittest.py
@@ -2,7 +2,7 @@
 
 import unittest 
 
-from atividades import comer, dormir
+from atividades import comer, dormir, eh_engracada
  
 class Atividadesunitest(unittest.TestCase):
     def test_comer(self):
@@ -12,7 +12,11 @@ class Atividadesunitest(unittest.TestCase):
     def test_dormir (self):
         self.assertEqual(dormir('gato', True), 'gato está dormindo')
         self.assertEqual(dormir('gato', False), 'gato está dormindo')
-    
+        
+    def test_eh_engracada (self):
+        self.assertEqual(eh_engracada('gato', True), 'gato está engracada')
+    #   self.assertFalse(eh_engracada('gato', False), 'gato não está engracada')
+  
  
 if __name__ == '__main__':
   unittest.main()
