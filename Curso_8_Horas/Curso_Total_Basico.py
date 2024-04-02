90293ed57c2683a4bb3e68b586462e2a847828f1
leonardo <leonnardo.campos@gmail.com>
2024-04-02 11:31:49 -0300

    Update Curso_Total_Basico.py

--- Curso_8_Horas/Curso_Total_Basico.py
+++ Curso_8_Horas/Curso_Total_Basico.py
@@ -72,4 +72,30 @@ print(1<=1) # True
 print(1<=2) # True
 
 ###############################################################
+# Operadores de atribuição (=, +=, -=, *=, /=, %=, **=, //=)
 
+x = y = z = False
+n1 = n2 = 0
+
+print('Digite um numero:')
+n1 = int(input())
+n2 = int(input('Digite outro numero:'))
+
+x = n1 == n2
+print('São iguais?', x, '\n')
+
+z = n1 > n2
+print(n1, ' É maior que ', n2, '?', z, '\n')
+
+y = n1 != n2
+print('São diferentes?', y, '\n') 
+###############################################################
+
+# Operadores logicos (and, or, not)
+
+x = True
+y = False
+
+print(x and y) # False
+print(x or y) # True
+print(not x) # False
