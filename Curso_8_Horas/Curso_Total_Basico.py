7f3597da2ef95fc0370fd177102c5cb895c69619
leonardo <leonnardo.campos@gmail.com>
2024-04-08 12:57:36 -0300

    up

--- Curso_8_Horas/Curso_Total_Basico.py
+++ Curso_8_Horas/Curso_Total_Basico.py
@@ -145,10 +145,16 @@
 #     print(num)
 
 
-lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-for num in lista:
-    print(num)
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] # 0 1 2 3 4 5 6 7 8 9
+for num in lista: # 1 2 3 4 5 6 7 8 9
+    print(num) # 1 2 3 4 5 6 7 8 9
 
-for num in range(0, 10):
-    print(num)
+for num in range(0, 10): # 0 1 2 3 4 5 6 7 8 9
+    print(num) # 0 1 2 3 4 5 6 7 8 9
+
+for num in range(0, 10, 2): # 0 2 4 6 8
+    print(num) # 0 2 4 6 8
      
+nome = input("Digite seu nome: ") # 0 1 2 3 4 5 6 7 8 9
+for x in range(10): # 0 1 2 3 4 5 6 7 8 9
+    print(f'{x+1} {nome}')
