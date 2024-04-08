5e97ebb70e0f5b05c59826b9d94e12e6a1ee90e5
leonardo <leonnardo.campos@gmail.com>
2024-04-08 16:57:50 -0300

    up

--- Curso_8_Horas/Curso_Total_Basico.py
+++ Curso_8_Horas/Curso_Total_Basico.py
@@ -145,16 +145,56 @@
 #     print(num)
 
 
-lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] # 0 1 2 3 4 5 6 7 8 9
-for num in lista: # 1 2 3 4 5 6 7 8 9
-    print(num) # 1 2 3 4 5 6 7 8 9
+# lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] # 0 1 2 3 4 5 6 7 8 9
+# for num in lista: # 1 2 3 4 5 6 7 8 9
+#     print(num) # 1 2 3 4 5 6 7 8 9
 
-for num in range(0, 10): # 0 1 2 3 4 5 6 7 8 9
-    print(num) # 0 1 2 3 4 5 6 7 8 9
+# for num in range(0, 10): # 0 1 2 3 4 5 6 7 8 9
+#     print(num) # 0 1 2 3 4 5 6 7 8 9
 
-for num in range(0, 10, 2): # 0 2 4 6 8
-    print(num) # 0 2 4 6 8
+# for num in range(0, 10, 2): # 0 2 4 6 8
+#     print(num) # 0 2 4 6 8
      
-nome = input("Digite seu nome: ") # 0 1 2 3 4 5 6 7 8 9
-for x in range(10): # 0 1 2 3 4 5 6 7 8 9
-    print(f'{x+1} {nome}')
+# nome = input("Digite seu nome: ") # 0 1 2 3 4 5 6 7 8 9
+# for x in range(10): # 0 1 2 3 4 5 6 7 8 9
+#     print(f'{x+1} {nome}')
+
+# pedras = ('Rubi', 'Esmeralda', 'Safira', 'Diamante') # 0 1 2 3
+# for pedra in pedras: # 0 1 2 3
+#     if pedra == 'Diamante': # Se pedra for Diamante
+#         continue # Ignora o valor
+#     print(pedra) # 0 1 2 3
+
+#############################################################
+
+# laços_encadeados 
+
+# for cont_externo in range(1, 6):
+#     print(f' \nRodada  {cont_externo}') # \n quebra de linha \t tabulac{cont_externo})
+#     for cont_interno in range(5, 0, -1):
+#         print(f'valor  {cont_interno}) ') # \n quebra de linha \t tabulac{cont_externo})
+# print('Fim do laço!')
+
+# import random # Importando biblioteca random
+# num = random.randint(0, 10) # 0 1 2 3 4 5 6 7 8 9
+# print(num) # 0 1 2 3 4 5 6 7 8 9
+
+# for A in range(0, 10): # 0 1 2 3 4 5 6 7 8 9
+#     num = random.randint(0, 10) # 0 1 2 3 4 5 6 7 8 9
+#     print(num) # 0 1 2 3 4 5 6 7 8 9
+
+#############################################################
+
+# modulos 
+
+#import math # Importando biblioteca math
+# num = math.sqrt(81) # raiz quadrada
+# print(num) # exibindo o resultado
+
+#############################################################
+
+# modo random - modulo aleatório
+
+import random # Importando biblioteca random
+num = random.randint(0, 10) # 0 1 2 3 4 5 6 7 8 9
+print(num) # 0 1 2 3 4 5 6 7 8 9
