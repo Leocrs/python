2a7a27f1d9eb5f3f9ed91cb34919f5f1e3ba70b7
leonardo <leonnardo.campos@gmail.com>
2024-04-10 13:45:13 -0300

    up

--- Curso_8_Horas/Curso_Total_Basico.py
+++ Curso_8_Horas/Curso_Total_Basico.py
@@ -251,5 +251,40 @@ import random # Importando biblioteca random
 
 # Matematica 
 
+# funçoes built-in
+
+# valores = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# soma = sum(valores)
+# print(soma)
+# print(max(valores)) # Retorna o maior valor da lista
+# print(min(valores)) # Retorna o menor valor da lista
+# print(pow(2, 3)) # Retorna o valor de 2 elevado a 3
+# print(round(sum(valores)/len(valores))) # Retorna a media da lista
+
+
+# import math
+
+# x = 8
+# y = 100
+
+# raiz_x = math.sqrt(x)
+# raiz_y = math.sqrt(y)
+
+# print(raiz_x)
+# print(raiz_y)
+# print(math.pi) # Retorna o valor de pi
+# print(math.floor(raiz_y)) # Arredonda para baixo
+# print(round(raiz_y)) # Arredonda para cima
+
+##############################################################
+
+# string 
+
+nome = 'Leonardo'
+sobrenome = 'Campos' 
+nome_completo = nome + ' ' + sobrenome # Concatenando string
+print(nome_completo)
+
+
 
 
