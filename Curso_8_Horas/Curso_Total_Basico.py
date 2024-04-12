639bdbb4a38627c4017f1b779c91926d6ba26b99
leonardo <leonnardo.campos@gmail.com>
2024-04-12 15:09:52 -0300

    up

--- Curso_8_Horas/Curso_Total_Basico.py
+++ Curso_8_Horas/Curso_Total_Basico.py
@@ -486,8 +486,32 @@ import random # Importando biblioteca random
 
 # Exceções - Tratamento de erros
 
-try: # bloco de tratamento de erros
-     x = int(input('digite um valor: '))
-     print(x)
-except ValueError: # bloco de tratamento de erros
-     print('Voce digitou um valor inválido')
+# try: # bloco de tratamento de erros
+#      x = int(input('digite um valor: '))
+#      print(x)
+# except ValueError: # bloco de tratamento de erros
+#      print('Voce digitou um valor inválido')
+
+def div(k, j):
+    return round(k / j, 2)
+
+if__name__ == '__main__':
+while True:
+        try:
+            n1 = int(input('digite um valor: '))
+            n2 = int(input('digite outro valor: '))
+            break
+        except ValueError:
+            print('Voce digitou um valor inválido')
+        try:
+            print(f'resultado: {div(n1, n2)}')
+        except ZeroDivisionError:
+            print('Impossível dividir por zero')
+
+# try: # bloco de tratamento de erros
+#     print(n1 / n2) # divisão
+# except ZeroDivisionError: # bloco de tratamento de erros
+#     print('Impossível dividir por zero')
+# else:
+#     print(f'resultado: {n1 / n2}')
+
