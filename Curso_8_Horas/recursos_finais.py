d7c4a9d1d01a3e55aff7c9c4c8877ffc72663801
leonardo <leonnardo.campos@gmail.com>
2024-04-15 11:40:42 -0300

    up

---
+++ Curso_8_Horas/recursos_finais.py
@@ -0,0 +1,11 @@
+var1 = 10
+var2 = 20
+
+# Trocar valores entre duas variáveis
+var2, var1 = var1, var2 # Trocar valores entre duas variáveis
+print('f Essa é a direção', var1, var2)
+
+# Operadores Condicionais Ternário
+menor = var1 if var1 < var2 else var2
+print(f' Esse é o menor valor {menor}')
+
