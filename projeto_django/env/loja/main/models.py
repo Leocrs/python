eca6441586c47d4e103bd5ddd4028e3e7f4e7784
leonardo <leonnardo.campos@gmail.com>
2024-04-18 14:04:54 -0300

    up

--- projeto_django/env/loja/main/models.py
+++ projeto_django/env/loja/main/models.py
@@ -1,3 +1,28 @@
+from distutils.command.upload import upload 
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=180)
+    image= models.ImageField(upload_to='category_images')
+
+    def __str__(self):
+        return self.title
+
+class Color(models.Model):
+    title = models.CharField(max_length=100)
+    Color_codigo= models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
+    
+class Marca(models.Model):
+    title = models.CharField(max_length=100)
+    image= models.ImageField(upload_to='category_images')
+    def __str__(self):
+        return self.title
+    
+class Size(models.Model):
+    title = models.CharField(max_length=100)
+    def __str__(self):
+        return self.title
+    
