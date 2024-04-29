a37ee7a89dda363ab938056ffa548f2f98ba70b0
leonardo <leonnardo.campos@gmail.com>
2024-04-29 16:23:37 -0300

    up

--- twtodos/.venv/todos/views.py
+++ twtodos/.venv/todos/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Olá TreinaWeb!!!")
+    return render(request, 'todos/home.html')
