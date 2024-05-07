a13b9b06d296f7ea50da726b40c1f0430a4c1aee
leonardo <leonnardo.campos@gmail.com>
2024-05-07 16:23:57 -0300

    up

--- twtodos/.venv/todos/views.py
+++ twtodos/.venv/todos/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
+from .models import Todo
 
 def todo_list(request):
-    nome = 'Leonardo'
-    alunos = ["Paulo", "Naty", "Ana", "Leo"] 
-    return render(request, 'todos/todo_list.html', {'nome': nome, 'alunos': alunos})
+    todos = Todo.objects.all()
+    return render(request, 'todos/todo_list.html' , {'todos': todos})
