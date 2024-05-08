e15f92fbee930a3bbd16366de9e8e6045b40b8d2
leonardo <leonnardo.campos@gmail.com>
2024-05-08 14:07:30 -0300

    up

--- twtodos/.venv/setup/settings.py
+++ twtodos/.venv/setup/settings.py
@@ -30,15 +30,23 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
-    'django.contrib.admin',
+DJANGO_APPS = ['django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'todos.apps.TodosConfig',
+    'django.contrib.staticfiles',]
+
+THIRD_PARTY_APPS = ['crispy_forms',
+    'crispy_bootstrap5',
 ]
+
+MY_APPS = ['todos.apps.TodosConfig',]
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + MY_APPS
+    
+    
+    
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -122,3 +130,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
