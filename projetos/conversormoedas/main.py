55969f8df765d57d8e64c048adfd4d5bcd3624b5
Leocrs <leonnardo.campos@gmail.com>
2024-06-14 13:29:05 -0300

    up

--- projetos/conversormoedas/main.py
+++ projetos/conversormoedas/main.py
@@ -17,11 +17,34 @@ janela = CTk() # janela
 janela.geometry = ("500x500") # tamanho da janela
 
 # Criar os botoes, texturas e outros elementos
-titulo = customtkinter.CTkLabel(janela) # Label para o texto Conversor de moedas
-texto_moeda_origem = customtkinter.CTkLabel(janela) # Label para selecionar a moeda de origem
+titulo = customtkinter.CTkLabel(janela, text="Conversor de moedas", font=("Arial", 20)) # Label para o texto Conversor de moedas
+texto_moeda_origem = customtkinter.CTkLabel(janela, text="Selecione a moeda de origem") # Label para selecionar a moeda de origem
+texto_moeda_destino = customtkinter.CTkLabel(janela, text="Selecione a moeda de destino") # Label para selecionar a moeda de destino
+campo_moeda_origem = customtkinter.CTkOptionMenu(janela, values=["USD", "BRL", "EUR", "BTC"]) # Campo para selecionar a moeda de origem
+campo_moeda_destino = customtkinter.CTkOptionMenu(janela, values=["USD", "BRL", "EUR", "BTC"]) # Campo para selecionar a moeda de destino
+botao_converter = customtkinter.CTkButton(janela, text="Converter") # Botão para converter
+
+def converter_moeda():
+    print("Conversor de moedas")    
+
+botao_converter = customtkinter.CTkButton(janela, text="Converter", command=converter_moeda) # Botão para converter
+
+lista_moedas = customtkinter.CTkScrollableFrame(janela) # Lista para exibir as moedas
+
+moedas_disponiveis = ["USD: Dolar americano", "BRL: Real brasileiro", "EUR: Euro", "BTC: Bitcoin"] # Lista de moedas disponíveis
+for moeda in moedas_disponiveis:
+    texto_moeda = customtkinter.CTkLabel(lista_moedas, text=moeda) # Label para exibir as moedas disponíveis
+    texto_moeda.pack()
 
 
 # Colocar todos os elementos na tela 
+titulo.pack(padx=20, pady=20) # Colocar o label Conversor de moedas
+texto_moeda_origem.pack(padx=20, pady=10) # Colocar o label Selecione a moeda de origem
+campo_moeda_origem.pack(padx=20) # Colocar o campo para selecionar a moeda de origem
+texto_moeda_destino.pack(padx=20, pady=10) # Colocar o label Selecione a moeda de destino
+campo_moeda_destino.pack(padx=20) # Colocar o campo para selecionar a moeda de destino
+botao_converter.pack(padx=20, pady=20) # Colocar o botão Converter
+lista_moedas.pack(padx=20, pady=20) # Colocar a lista para exibir as moedas
 
 
 # Executar a janela
