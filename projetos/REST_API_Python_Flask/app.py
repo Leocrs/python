8f53a1a47cc6c013794f0378b61b26202c9fe32a
Leonardo <leonnardo.campos@gmail.com>
2024-08-31 16:45:32 -0300

    up

--- projetos/REST_API_Python_Flask/app.py
+++ projetos/REST_API_Python_Flask/app.py
@@ -5,20 +5,16 @@ app = Flask(__name__)
 api = Api(app)
 
 hoteis = [
-    
-hoteis = [
     {'hotel_id': 'alpha', 'nome': 'Alpha Hotel', 'estrelas': 4.3,'diaria': 420.34, 'cidade': 'Rio de Janeiro'},
     {'hotel_id': 'bravo', 'nome': 'bravo Hotel', 'estrelas': 4.3, 'diaria': 420.34, 'cidade': 'São Paulo'},
     {'hotel_id': 'charle', 'nome': 'charle Hotel', 'estrelas': 4.3, 'diaria': 420.34, 'cidade': 'Belo Horizonte'},
 ]
 
-
 class Hoteis(Resource):
     def get(self):
-        return {'hoteis': 'hoteis'}   
+        return {'hoteis': hoteis}  # Return the actual list of hotels
+
 api.add_resource(Hoteis, '/hoteis')
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-    
