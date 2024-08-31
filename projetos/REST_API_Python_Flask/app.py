de7e5e48a78b3ef0af70eaea9062160c54635243
Leonardo <leonnardo.campos@gmail.com>
2024-08-31 16:09:55 -0300

    up

--- projetos/REST_API_Python_Flask/app.py
+++ projetos/REST_API_Python_Flask/app.py
@@ -1,13 +1,17 @@
-from flask import Flask
+from flask import Flask as Flask
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
 
+hoteis = [{'hotel_id': 'alpha', 'nome': 'Alpha Hotel',
+             'estrelas': 4.3, 'diaria':
+               420.34, 'diaria_completo': 498.40},]
+
+
 class Hoteis(Resource):
     def get(self):
-        return {'hoteis': 'meus hoteis'}
-    
+        return {'hoteis': 'meus hoteis'}   
 api.add_resource(Hoteis, '/hoteis')
 
 if __name__ == '__main__':
