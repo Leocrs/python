a93e120b116eb6c19962c4c9e5e48cda74037bec
Leonardo <leonnardo.campos@gmail.com>
2024-09-05 11:16:27 -0300

    up

--- projeto_flask/app2.py
+++ projeto_flask/app2.py
@@ -1,21 +1,14 @@
 from flask import Flask
 
-app2 = Flask(__name__)
+app = Flask(__name__)
 
-@app2.route('/')
+@app.route("/")
 def index():
-    return "index"
+    return "Hello, World!"
 
-def teste():
-    return "<p> testando 1 </p>"
+@app.route("/favicon.ico")
+def favicon():
+    return "Favicon"
 
-def teste2():
-    return "<h1> testando 2 </h1>"
-
-
-app2.add_url_rule('/teste', 'teste', teste)
-app2.add_url_rule('/teste-2', 'teste2', teste2)
-
-
-if __name__ == '__main__':
-    app2.run(debug=True, port=3000) # debug=True para mostrar erros de test de forma visível e automatica
+if __name__ == "__main__":
+    app.run()
---
+++ projeto_flask/app3.py
@@ -0,0 +1,13 @@
+from flask import Flask, redirect, url_for
+
+app = Flask(__name__)
+
+app.route('/admin')
+def admin():
+    return '<h1>Admin</h1>'
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
