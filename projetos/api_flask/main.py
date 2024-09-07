e4989da853311f518ba983cc71547895e47ef0a2
Leonardo <leonnardo.campos@gmail.com>
2024-09-07 18:41:42 -0300

    up

--- projetos/api_flask/main.py
+++ projetos/api_flask/main.py
@@ -1,10 +1,14 @@
-from flask import Flask # Importa o Flask
+from flask import Flask, make_response # Importa o Flask e o make_response
+from bd import Carros # Importa o bd carros
 
 
 app = Flask(__name__) # Cria a aplicação Flask
 
 
-app.run() # Roda a aplicação Flask
+@app.route('/carros', methods=["GET"]) # Cria uma rota para retornar os carros
+def get_carros():
+    return make_response(Carros) # Retorna os carros no formato json
 
+app.run() # Roda a aplicação Flask
 
 
