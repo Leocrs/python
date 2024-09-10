d51e9e47f13bb34cdc72c55befb8591b81772965
Leonardo <leonnardo.campos@gmail.com>
2024-09-09 21:57:09 -0300

    up

--- projetos/api_flask/main.py
+++ projetos/api_flask/main.py
@@ -1,14 +1,23 @@
-from flask import Flask, make_response # Importa o Flask e o make_response
-from bd import Carros # Importa o bd carros
+from flask import Flask, make_response, jsonify, request
+# Importa o Flask e o make_response
 
+from bd import Carros
 
-app = Flask(__name__) # Cria a aplicação Flask
+app = Flask(__name__) 
+app.config['json_sort_keys'] = False # Evita que os dicionários sejam ordenados
 
-
-@app.route('/carros', methods=["GET"]) # Cria uma rota para retornar os carros
+@app.route('/carros', methods=["GET"]) 
 def get_carros():
-    return make_response(Carros) # Retorna os carros no formato json
+    return make_response(jsonify(Carros)) 
 
-app.run() # Roda a aplicação Flask
+@app.route('/carros', methods=["POST"])
+def create_carros():
+    try:
+        carro = request.json
+        Carros.append(carro)
+        return carro
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
 
+app.run() 
 
