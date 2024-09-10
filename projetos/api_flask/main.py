b15024abe353e217ca5b764c82a5aed9260d8ac1
Leonardo <leonnardo.campos@gmail.com>
2024-09-10 09:20:19 -0300

    up

--- projetos/api_flask/main.py
+++ projetos/api_flask/main.py
@@ -1,23 +1,28 @@
 from flask import Flask, make_response, jsonify, request
-# Importa o Flask e o make_response
-
 from bd import Carros
 
 app = Flask(__name__) 
-app.config['json_sort_keys'] = False # Evita que os dicionários sejam ordenados
+app.config['json_sort_keys'] = False 
 
 @app.route('/carros', methods=["GET"]) 
 def get_carros():
-    return make_response(jsonify(Carros)) 
+    return make_response(
+        jsonify(
+            mensagem = 'Lista de carros',
+            dados = Carros                 
+      ) 
+  )
 
 @app.route('/carros', methods=["POST"])
 def create_carros():
-    try:
-        carro = request.json
-        Carros.append(carro)
-        return carro
-    except Exception as e:
-        return jsonify({"error": str(e)}), 400
+    carro = request.json
+    Carros.append(carro)
+    return make_response(
+        jsonify(
+            mensagem = 'Carro criado com sucesso!',
+        )
+    )    
+    
 
 app.run() 
 
