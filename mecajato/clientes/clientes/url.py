ebaa748c0573dc92aaa613134213bd9ebebdcad5
Leonardo <leonnardo.campos@gmail.com>
2024-09-24 13:12:04 -0300

    up

--- mecajato/clientes/clientes/url.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include, url
-from . import views
-
-urlpatterns = [
-    path(" ", views.index, name="index"),
-]
--- mecajato/clientes/views.py
+++ mecajato/clientes/views.py
@@ -1,7 +1,3 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-def clientes(request):
-    return HttpResponse("Estou em clientes")
-
-
+# Create your views here.
