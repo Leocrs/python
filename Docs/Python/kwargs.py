9546e4ed3eef741a998ff9c34f077f7b9b96396b
Leonardo <leonnardo.campos@gmail.com>
2024-12-18 18:26:44 -0300

    up

--- Docs/Python/kwargs.py
+++ Docs/Python/kwargs.py
@@ -1,4 +1,3 @@
-
 #**kwargs - keyword arguments é um parâmetro que recebe uma lista de valores.
 #Por convensão chamamos de **kwargs. 
 #Diferente de **args, **kwargs recebem uma lista de valores nomedados. e transformam em um dicionário.
@@ -6,40 +5,39 @@
 #Exemplo
 
 def cores_favoritas(**kwargs):
-   # for cores in kwargs.values(): #kwargs recebe uma lista de valores
+    """Imprime as cores favoritas de cada pessoa."""
     for pessoa, cores in kwargs.items():
-        print (f' A cor favorita de {pessoa.title()} é {cores}')
-        #print(cores)
-    
+        print(f'A cor favorita de {pessoa.title()} é {cores}')
+
 cores_favoritas(Leonardo='preto', Natalia='Rosa', Paula='Amarelo')
-#print(cores_favoritas(Leonardo='preto', Natalia='vermelho', Paula='azul'))
-   
+
 def cumprimento_especial(**kwargs):
+    """Retorna uma mensagem especial baseada nos argumentos fornecidos."""
     if "Leonardo" in kwargs and kwargs["Leonardo"] == "Estudante de Python":
-        return 'Você está estudando Python pra valer cara!'
+        return 'Você está estudando Python pra valer cara!'
     elif "Leonardo" in kwargs:
-        return f'Você está estudando {kwargs["Leonardo"]}'
-    return 'Você não está estudando Python direito!'
+        return f'Você está estudando {kwargs["Leonardo"]}'
+    return 'Você não está estudando Python direito!'
 
 print(cumprimento_especial())
 print(cumprimento_especial(Leonardo='Estudante de Python'))
 print(cumprimento_especial(Leonardo='Uauu'))
-print(cumprimento_especial(Leonardo='e seu futuro será especial!!!'))  
+print(cumprimento_especial(Leonardo='e seu futuro será especial!!!'))
 
-
-def minha_fucao(idade, nome, *args, solteiro=False, **kwargs):
-    print(f'{nome} a cima {idade} anos')
+def minha_funcao(idade, nome, *args, solteiro=False, **kwargs):
+    """Imprime informações sobre a pessoa."""
+    print(f'{nome} acima de {idade} anos')
     print(*args)
     if solteiro:
         print('Solteiro')
     else:
         print('Casado')
     print(kwargs)
-    
-minha_fucao(46, "Leonardo")
-minha_fucao(18,'Felicidade', 4,3,2,1, solteiro=True)
-minha_fucao(38,'Natália', 'Eu=Não, você=Sim')
-minha_fucao(46, "Leonardo", java=False, python=True) 
+
+minha_funcao(46, "Leonardo")
+minha_funcao(18, 'Felicidade', 4, 3, 2, 1, solteiro=True)
+minha_funcao(38, 'Natália', 'Eu=Não, você=Sim')
+minha_funcao(46, "Leonardo", java=False, python=True)
 
 # Nas nossas funçoes podem ter:(NESSA ORDEM DE EXECUCAO):
 # Parametros obrigatorios
@@ -47,14 +45,17 @@ minha_fucao(46, "Leonardo", java=False, python=True)
 # kwargs
 #Entenda o porque dessa ordem dos parametros é importante
 # Função com a ordem de execução incorreta de parametros
-def mostra_informacao (a, b,*args, Estudante='Leonardo', **kwargs):
+def mostra_informacao(a, b, *args, Estudante='Leonardo', **kwargs):
+    """Retorna uma lista com as informações fornecidas."""
     return [a, b, args, Estudante, kwargs]
 
-print(mostra_informacao(1,2,3, Estudante='Leonardo',Cargo='Programador'))
+print(mostra_informacao(1, 2, 3, Estudante='Leonardo', Cargo='Programador'))
 
 def mostra_nomes(**kwargs):
-   return (f'{kwargs["nome"]} {kwargs["sobrenome"]}')
+    """Retorna o nome completo baseado nos argumentos fornecidos."""
+    return f'{kwargs["nome"]} {kwargs["sobrenome"]}'
+
 print(mostra_nomes(nome='Leonardo', sobrenome='Campos'))
 
-nomes = {'nome':'Leonardo', 'sobrenome':'Campos'}
+nomes = {'nome': 'Leonardo', 'sobrenome': 'Campos'}
 print(mostra_nomes(**nomes))
