a2512e814fd7a17816b871ccc80377044a32b8a2
Leonardo <leonnardo.campos@gmail.com>
2024-12-20 16:26:38 -0300

    up

--- Analise_base_vensdas.py
+++ Analise_base_vensdas.py
@@ -3,11 +3,10 @@ import pandas as pd
 # Carregar a base de dados
 df = pd.read_excel(r'C:\Users\Leonardo\OneDrive\Documentos\Aula 4 Hashtag\Base de Vendas.xlsx')
 
-# Exibir as colunas do DataFrame
-print(df.columns)
-
 # Agrupar por categoria de produtos e somar a quantidade de vendas
 vendas_por_categoria = df.groupby('categoria')['vendas'].sum()
 
 # Exibir o resultado
 print(vendas_por_categoria)
+
+
