af4701401f95f241a61ab62582087d92929ae588
Leonardo <leonnardo.campos@gmail.com>
2024-12-20 17:03:54 -0300

    up

--- Analise_base_vensdas.py
+++ Analise_base_vensdas.py
@@ -1,12 +1,22 @@
 import pandas as pd
 
-# Carregar a base de dados
+# Ler a planilha de base de vendas
 df = pd.read_excel(r'C:\Users\Leonardo\OneDrive\Documentos\Aula 4 Hashtag\Base de Vendas.xlsx')
+
+# Verificar os nomes das colunas
+print(df.columns)
+
+# Adicionar uma coluna 'estado' à planilha
+df['estado'] = df['cidade'].apply(lambda x: 'SP' if x == 'São Paulo' else 'Outro')
 
 # Agrupar por categoria de produtos e somar a quantidade de vendas
 vendas_por_categoria = df.groupby('categoria')['vendas'].sum()
 
+# Agrupar por estado e somar a quantidade de vendas
+vendas_por_estado = df.groupby('estado')['vendas'].sum()
+
 # Exibir o resultado
+print("Vendas por categoria:")
 print(vendas_por_categoria)
-
-
+print("\nVendas por estado:")
+print(vendas_por_estado)
